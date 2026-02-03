08f84e0f8c89eb1d81ade75b4e965caf23c268aa
Domen <domen.kovacic01@gmail.com>
2026-02-03 09:38:42 +0100

    fixed project structure - removed nested folder

---
+++ src/main/java/com/domen/game_session_management_api/GameSessionManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.domen.game_session_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSessionManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSessionManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domen/game_session_management_api/GameSessionManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domen.game_session_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSessionManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
